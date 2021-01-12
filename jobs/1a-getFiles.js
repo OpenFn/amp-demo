cd1e6eecb86b940d6febafe6ba09c856c5745694
Chaiwa Berian <chaiwa.berian@gmail.com>
2021-01-12 14:48:35 +0200

    Ready for testing

--- jobs/1a-getFiles.js
+++ jobs/1a-getFiles.js
@@ -1,34 +1,56 @@
 query(
   "SELECT Title, Id, FirstPublishLocationId, IsLatest, FileExtension, FileType, PathOnClient, ContentModifiedDate, ContentUrl, Description, VersionData FROM ContentVersion WHERE (Title LIKE '%annual%' AND Title LIKE '%report%') OR (Title LIKE '%m&e%')"
 );
-alterState((state) => {
+alterState(state => {
   const data = {
-    files: state.references[0].records.map((i) => ({
+    files: state.references[0].records.map(i => ({
       id: `${i.Id}/VersionData`,
-      //   Adding these for next job
+      fileName: i.Title,
       fileExtension: i.FileExtension,
-      projectName: "Breaking Barriers Karonga Project",
+      projectName: 'Breaking Barriers Karonga Project',
       folderName:
-        i.Title.toLowerCase().includes("annual") &&
-        i.Title.toLowerCase().includes("report")
-          ? "Annual Reports"
-          : "M&E",
+        i.Title.toLowerCase().includes('annual') &&
+        i.Title.toLowerCase().includes('report')
+          ? 'Annual Reports'
+          : 'M&E',
     })),
   };
   return { ...state, data };
 });
 each(
-  dataPath("files[*]"),
-  retrieve("ContentVersion", dataValue("id"), (state) => {
+  dataPath('files[*]'),
+  retrieve('ContentVersion', dataValue('id'), state => {
     const file = state.data;
-    let data = {
-      ...file,
-      blob: state.references[0],
-    };
-    // return axios.request(state.configuration.inboxUrl, {
-    //   method: "post",
-    //   data: data,
-    // });
-    return { ...state, data: data, references: [...state.references, data] };
+    const blob = state.references[0];
+
+    return axios
+      .request(
+        `https://login.microsoftonline.com/${state.configuration.tenant}/oauth2/v2.0/token`,
+        {
+          method: 'post',
+          data: state.configuration.clientSecrets,
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            Host: 'login.microsoftonline.com',
+          },
+        }
+      )
+      .then(result => {
+        const url = `https://graph.microsoft.com/v1.0/sites/openfn.sharepoint.com,848825d7-4e10-4ebd-867c-d3621d5c52d6,7294c260-9f2a-462b-bcd9-faf0e9c1f70f/drives/b!1yWIhBBOvU6GfNNiHVxS1mDClHIqnytGvNn68OnB9w9OBF3Wt6sIQ484NzClqrYF/root:/${file.projectName}/${file.folderName}/${file.fileName}.${file.fileExtension}:/content`;
+
+        return axios.request(url, {
+          method: 'put',
+          data: blob,
+          headers: {
+            Authorization: 'Bearer ' + result.data.access_token,
+          },
+        });
+      })
+      .then(uploadResponse => {
+        console.log('Res', uploadResponse);
+        return state;
+      });
   })
 );
+
+alterState(state => ({ ...state, references: [] }));
