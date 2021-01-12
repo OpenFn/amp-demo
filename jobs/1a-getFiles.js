dc57d41f756fdabb39736e6a9a04eae5d1b18014
Aleksa Krolls <aleksagk@gmail.com>
2021-01-12 11:31:54 +0200

    chaiwa@openfn.org's change on OpenFn.org

--- jobs/1a-getFiles.js
+++ jobs/1a-getFiles.js
@@ -1,3 +1,17 @@
+// Your job goes here.
+
 query(
   "SELECT Title, FirstPublishLocationId, IsLatest, FileExtension, FileType, PathOnClient, ContentModifiedDate, ContentUrl, Description, VersionData FROM ContentVersion WHERE (Title LIKE '%annual%' AND Title LIKE '%report%') OR (Title LIKE '%m&e%')"
 );
+
+query(...getsomefiles);
+alterState(state => {
+  const files = state.references.filter(r => r.fileName === 'something')
+  return { ...state, files }
+});
+each(
+  '$.files[*]',
+  alterState(state => {
+    return axios.post(...).then(() => state);
+  })
+)
