ed46eb78da5ddbe6ccf412878ae04075ed5376fc
Chaiwa Berian <chaiwa.berian@gmail.com>
2021-01-12 13:56:52 +0200

    Update get expression, for #1

--- jobs/1a-getFiles.js
+++ jobs/1a-getFiles.js
@@ -1,5 +1,3 @@
-// Your job goes here.
-
 query(
   "SELECT Title, Id, FirstPublishLocationId, IsLatest, FileExtension, FileType, PathOnClient, ContentModifiedDate, ContentUrl, Description, VersionData FROM ContentVersion WHERE (Title LIKE '%annual%' AND Title LIKE '%report%') OR (Title LIKE '%m&e%')"
 );
@@ -22,21 +20,15 @@ alterState((state) => {
 each(
   dataPath("files[*]"),
   retrieve("ContentVersion", dataValue("id"), (state) => {
-    const blob = state.references[0];
-    const file = { ...state.data, blob };
-    // At this point I wanna post to OpenFn inbox
-    return { ...state, data: file, references: [...state.references, file] };
+    const file = state.data;
+    let data = {
+      ...file,
+      blob: state.references[0],
+    };
+    return axios.request(state.configuration.inboxUrl, {
+      method: "post",
+      data: data,
+    });
+    // return { ...state, data: data, references: [...state.references, data] };
   })
 );
-
-query(...getsomefiles);
-alterState(state => {
-  const files = state.references.filter(r => r.fileName === 'something')
-  return { ...state, files }
-});
-each(
-  '$.files[*]',
-  alterState(state => {
-    return axios.post(...).then(() => state);
-  })
-)
